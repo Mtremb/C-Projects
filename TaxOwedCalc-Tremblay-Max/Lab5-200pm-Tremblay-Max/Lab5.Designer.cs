32a3a36beff60ee0eaa92585dfc21db7918b1d92
Max Tremblay <124537704+Mtremb@users.noreply.github.com>
2023-02-06 11:48:35 -0600

    Add files via upload

---
+++ TaxOwedCalc-Tremblay-Max/Lab5-200pm-Tremblay-Max/Lab5.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Lab5_200pm_Tremblay_Max
+{
+    partial class Lab5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTaxableIncome = new System.Windows.Forms.TextBox();
+            this.txtTaxOwed = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(85, 124);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(189, 124);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Taxable income:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Income tax owed:";
+            // 
+            // txtTaxableIncome
+            // 
+            this.txtTaxableIncome.Location = new System.Drawing.Point(164, 21);
+            this.txtTaxableIncome.Name = "txtTaxableIncome";
+            this.txtTaxableIncome.Size = new System.Drawing.Size(100, 20);
+            this.txtTaxableIncome.TabIndex = 4;
+            // 
+            // txtTaxOwed
+            // 
+            this.txtTaxOwed.Location = new System.Drawing.Point(164, 61);
+            this.txtTaxOwed.Name = "txtTaxOwed";
+            this.txtTaxOwed.ReadOnly = true;
+            this.txtTaxOwed.Size = new System.Drawing.Size(100, 20);
+            this.txtTaxOwed.TabIndex = 5;
+            // 
+            // Lab5
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(315, 169);
+            this.Controls.Add(this.txtTaxOwed);
+            this.Controls.Add(this.txtTaxableIncome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Lab5";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Forever Fitness Tax Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTaxableIncome;
+        private System.Windows.Forms.TextBox txtTaxOwed;
+    }
+}
+
---
+++ TaxOwedCalc-Tremblay-Max/Lab5-200pm-Tremblay-Max/Lab5.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5_200pm_Tremblay_Max
+{
+    public partial class Lab5 : Form
+    {
+        public Lab5()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            //closes the application
+            this.Close();
+
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+
+            //declare the variable to store income and tax
+            Decimal income = Decimal.Parse(txtTaxableIncome.Text);
+            decimal tax = .0m;
+
+            //use if else statements to comopute the tax amount
+            if (income <= 9250)
+                tax = (int)(income * .10m);
+            else if (income > 9250 && income <= 37500)
+                tax = 925.50m + (int)((income - 9250) * .15m);
+            else if (income > 37500 && income <= 90750)
+                tax = 5150.25m + (int)((income - 37500) * .25m);
+            else if (income > 90750 && income <= 189400)
+                tax = 18490.25m + (int)((income - 90750) * .28m);
+            else if (income > 189400 && income <= 411500)
+                tax = 46075.25m + (int)((income - 189400) * .33m);
+            else if (income > 411500 && income <= 413200)
+                tax = 119401.25m + (int)((income - 411500) * .35m);
+            else if (income > 413200)
+                tax = 119998.25m + (int)((income - 413200) * .396m);
+
+            //convert tax to string and display 
+            txtTaxOwed.Text = tax.ToString("c");
+
+            //keep focus on the input box
+            txtTaxableIncome.Focus();
+        }
+    }
+}
